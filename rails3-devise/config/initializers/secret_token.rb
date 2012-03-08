a68c7d07c4bbb43d4e91c42a34c8f51ecd52b39d
Jignesh Gohel <jignesh.ghl@gmail.com>
2012-03-08 10:07:52 +0530

    Initial commit for a Rails 3 application demonstrating Ruby Gem Devise.

---
+++ rails3-devise/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3devise::Application.config.secret_token = '9c0f297e18aa52a2eb6c889e89f805bdca8513c8e550a865cf6e4591641bf630249ce87569417242a0d424a462728d4548049ebed2b5f30cccfa732105fd384b'
